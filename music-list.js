add3f885507a5e45bc64a4830c96c437513c6e09
PhamHuuViet <phamhuuviet2410@gmail.com>
2024-12-22 14:15:40 +0700

    new version Soundsoul

---
+++ music-list.js
@@ -0,0 +1,125 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "The Best Of Bruno Mars",
+    artist: "Bruno Mars",
+    img: "bruno",
+    src: "bruno"
+  },
+  {
+    name: "Lofi Chill 1",
+    artist: "Hạ Sang",
+    img: "lofi-1",
+    src: "lofi-1"
+  },
+  {
+    name: "Lofi Chill 2",
+    artist: "Hạ Sang",
+    img: "lofi-2",
+    src: "lofi-2"
+  },
+  {
+    name: "Lofi Chill 3",
+    artist: "Hạ Sang",
+    img: "lofi-3",
+    src: "lofi-3"
+  },
+  {
+    name: "Lofi Chill 4",
+    artist: "Hạ Sang",
+    img: "lofi-4",
+    src: "lofi-4"
+  },
+  {
+    name: "Playlist Lofi Tết ",
+    artist: "Hạ Sang",
+    img: "tet",
+    src: "tet"
+  },
+  {
+    name: "Playlist CM1X - 1 ",
+    artist: "CM1X Official",
+    img: "cm1x-1",
+    src: "cm1x-1"
+  },
+  {
+    name: "Playlist Mưa - 1 ",
+    artist: "Dịu Kha",
+    img: "mua-1",
+    src: "mua-1"
+  },
+  {
+    name: "Playlist Mưa - 2 ",
+    artist: "Dịu Kha",
+    img: "mua-2",
+    src: "mua-2"
+  },
+  {
+    name: "Playist Dewie",
+    artist: "Dewie",
+    img: "dewie",
+    src: "dewie"
+  },
+  {
+    name: "Playist Dewie 2",
+    artist: "Dewie",
+    img: "dewie-2",
+    src: "dewie-2"
+  },
+  {
+    name: "PLAYLIST TAYNGUYENSOUND HIT TRACKS",
+    artist: "TAYNGUYENSOUND ",
+    img: "taynguyensound",
+    src: "taynguyensound"
+  },
+  {
+    name: "Bạn Có Tài Mà Collection #1",
+    artist: "Bạn Có Tài Mà ",
+    img: "bancotaima-1",
+    src: "bancotaima-1"
+  },
+  {
+    name: "Bạn Có Tài Mà Collection #2",
+    artist: "Bạn Có Tài Mà ",
+    img: "bancotaima-2",
+    src: "bancotaima-2"
+  },
+  {
+    name: "Đây đơn giản là Playlist của Vũ.",
+    artist: "Vũ",
+    img: "vu",
+    src: "vu"
+  },
+  {
+    name: "Những Ca Khúc Hay Nhất Của Andiez, Jaykii,..",
+    artist: "Andiez, Jaykii,...",
+    img: "andiez",
+    src: "andiez"
+  },
+  {
+    name: "Những Ca Khúc Hay Nhất Của JUSTATEE",
+    artist: "Justatee",
+    img: "justatee",
+    src: "justatee"
+  },
+  {
+    name: "Những Ca Khúc Hay Nhất Của RHYMASTIC",
+    artist: "Rhymastic",
+    img: "rhymastic",
+    src: "rhymastic"
+  },
+  {
+    name: "Playlist của Chillies",
+    artist: "Chillies",
+    img: "chillies",
+    src: "chillies"
+  },
+];
